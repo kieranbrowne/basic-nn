7182a3e1f403a3ea2229e755bb7ba467a6e45a8f
Kieran Browne <kierantbrowne@gmail.com>
2017-04-13 17:38:24 +1000

    Changed to a functional/composable solution.

--- src/basic_nn/core.clj
+++ src/basic_nn/core.clj
@@ -1,5 +1,5 @@
 (ns basic-nn.core
-  (:require [clojure.core.matrix :as matrix]
+  (:require [clojure.core.matrix :as matrix :refer [dot transpose exp]]
             [clojure.core.matrix.random :as random]
             [clojure.core.matrix.operators :refer :all]
             [clojure.math.numeric-tower :refer [expt abs round]]
@@ -39,113 +39,79 @@
 (def synapses-0 (atom (matrix-of random-synapse [3 5])))
 (def synapses-1 (atom (matrix-of random-synapse [5 1])))
 
-(defn activate
+(defn activation
   "Sigmoid function"
-  [x] (/ 1 (+ 1 (matrix/exp (- x)))))
+  [x] (/ 1 (+ 1 (exp (- x)))))
 
-(defn deactivate
+(defn derivative
   "Derivative of sigmoid"
   [x] (* x (- 1 x)))
 
 
-(defn step-forward
-  "For each step in feed forward, the new layer of neurons
-  are a function of the previous layer and it's synapses"
-  [neurons synapses]
-  (activate (matrix/dot neurons synapses)))
+(defn layer
+  "The layers in our network are a curried function of weights and inputs"
+  [weights]
+  (fn [inputs]
+    (activate (dot inputs @weights))))
 
-(defn feed-forward
-  [input & synapses]
-  (case (count synapses)
-    0 input ; if no synapses return just the input
-    1 (step-forward input @(first synapses))
-    ;; otherwise recurse
-    (apply
-     feed-forward
-     (into [(step-forward input @(first synapses))]
-           (rest synapses)))))
+(def network
+  "Our network is nothing but the composition of our layers.
+  Note that function composition is read right to left."
+  (comp (layer synapses-1) (layer synapses-0)))
+
+;; Thats all we need for forward propagation
+;; We can now apply our network as a function of our training input
+(network training-input)
+
+;; Training is the process of gradient descent
+;; For each layer we need to multiply the derivative of our layer function
+;; in relation to the weights by the size of the error.
+
+;; Because these are just functions we can run backwards as well as forwards
+;; and thus derive our input from out output.
+
+;; Just as the networks feed forward is the composition of layers,
+;; the network backward propagation
 
 
-(defn errors [training-outputs real-outputs]
-  (- training-outputs real-outputs))
+(defn deltas [cost-fn]
+  (reduce
+   (fn [deltas layer]
+     (conj deltas
+           (* (derivative layer)
+              (if (empty? deltas)
+                (cost-fn)
+                (dot (last deltas) (transpose @synapses-1))))))
+   [] [(network training-input) ((layer synapses-0) training-input)]))
 
-(defn output-deltas [targets outputs]
-  (* (deactivate outputs)
-     (- targets outputs)))
+(defn gradient-decent [cost-fn]
+  (let [deltas (deltas cost-fn)]
+    (swap! synapses-1 + (dot (transpose ((layer synapses-0) training-input))
+                             (first deltas)
+                             ))
+    (swap! synapses-0 + (dot (transpose training-input)
+                             (second deltas)))
+    (mean-error (errors))
+    ))
 
-(defn hidden-deltas [output-deltas neurons synapses]
-  (* (matrix/dot output-deltas (matrix/transpose synapses))
-     (deactivate neurons)
-     ))
 
-(defn apply-deltas [synapses neurons deltas learning-rate]
-  (+ synapses
-     (* learning-rate
-        (matrix/dot (matrix/transpose neurons) deltas))))
+(def errors (fn [] (- training-output (network training-input))))
 
 (defn mean-error [numbers]
   (let [absolutes (map abs (flatten numbers))]
     (/ (apply + absolutes) (count absolutes))))
 
-(defn train
-  "Train the network and return the error"
-  [training-input training-output]
-  (let [
-        hidden-layer (step-forward training-input @synapses-0)
-        output-layer (step-forward hidden-layer @synapses-1)
-        output-delta (output-deltas training-output output-layer)
-        hidden-delta (hidden-deltas output-delta hidden-layer @synapses-1)
-        output-error (errors training-output output-layer)
-        ]
-    (do
-      (swap! synapses-1
-             #(apply-deltas % hidden-layer output-delta 1))
-      (swap! synapses-0
-             #(apply-deltas % training-input hidden-delta 1))
-      (mean-error output-error)
-      )))
-
-(defn train-prime
-  "Train the network and return the error"
-  [times training-input training-output]
-  (loop [times times syn0 @synapses-0 syn1 @synapses-1]
-           (if (<= times 0)
-             (do ; set the syn vals
-               (reset! synapses-1 syn1)
-               (reset! synapses-0 syn0)
-               (mean-error
-                (errors training-output
-                        (feed-forward
-                         training-input
-                         synapses-0 synapses-1)))
-               )
-             (let [
-                   hidden-layer (step-forward training-input syn0)
-                   output-layer (step-forward hidden-layer syn1)
-                   output-delta (output-deltas training-output output-layer)
-                   hidden-delta (hidden-deltas output-delta hidden-layer syn1)
-                   ]
-                 (recur (dec times)
-                        (apply-deltas syn0 training-input hidden-delta 1)
-                        (apply-deltas syn1 hidden-layer output-delta 1)
-                        ))
-               )
-             ))
-
 
 ;; Train once
-(train training-input training-output)
+(gradient-decent errors)
 
 ;; Train 1000 passes
-(train-prime 10000 training-input training-output)
-
-;; Train 1000 passes
-(dotimes [i 10000]
-  (train training-input training-output))
+(dotimes [i 1000]
+  (gradient-decent errors))
 
 
 ;; Check results
-(feed-forward training-input synapses-0 synapses-1)
+(network training-input)
 
 ;; Check results for new values
-(feed-forward [0 1 0] synapses-0 synapses-1)
+(network [0 1 0])
